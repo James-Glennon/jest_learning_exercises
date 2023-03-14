dbe22a8ca873a41d0680a5b0c6191324b12350c6
James-Glennon <glennon.jjp91@gmail.com>
2023-03-14 17:56:20 +0000

    add tests for addition function

--- scripts/tests/calc.js
+++ scripts/tests/calc.js
@@ -1,5 +1,5 @@
-function addition(){
-    return 42;
+function addition(num1,num2){
+    return(typeof(num1 + num2) === "number"? num1 + num2: NaN)
 }
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,9 +1,22 @@
+
 const addition = require("./calc");
 
 describe("Calculator", () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
+        })
+        test("should return 77 for 44 + 33", () => {
+            expect(addition(44, 33)).toBe(77);
+        })
+        test("should return error for no value provided", () => {
+            expect(addition()).toBe(NaN);
+        })
+        test("should return error for one nonsense value", () => {
+            expect(addition("panda", 22)).toBe(NaN);
+        })
+        test("should return error for two nonsense value", () => {
+            expect(addition("panda", "moose")).toBe(NaN);
         })
     })
     /*
